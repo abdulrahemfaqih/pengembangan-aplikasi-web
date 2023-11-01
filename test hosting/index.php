48ad194199aabe09f757f9c033446256c8d77221
abdulrahemfaqih <faqih3935@gmail.com>
2023-11-01 12:31:13 +0700

    clear files adn folders

--- test hosting/index.php
+++
@@ -1,3 +0,0 @@
-<?php include "layout/header.php" ?>
-<h1 style="text-align: center; margin: 5rem;">Selamat Datang di Waroengku</h1>
-<?php include "layout/footer.php" ?>
--- web hosting warung/index.php
+++
@@ -1,3 +0,0 @@
-<?php include "layout/header.php" ?>
-<h1 style="text-align: center; margin: 5rem;">Selamat Datang di Warung Faqih</h1>
-<?php include "layout/footer.php" ?>
