31942f9a9e790650c51afae23f6a8b2cd3615024
abdulrahemfaqih <faqih3935@gmail.com>
2023-11-09 20:06:55 +0700

    add folder

---
+++ kuliah/pertemuan13/detil_order.php
@@ -0,0 +1,162 @@
+<?php
+require "functions.php";
+
+if (isset($_GET["id_order"])) {
+    $id_order = $_GET["id_order"];
+    $order = query("SELECT * FROM `order` WHERE id_order = $id_order")[0];
+    $order_detail = query("SELECT order_detil.id_order_detil, order_detil.status_order_detil, order_detil.id_order, order_detil.subtotal, order_detil.id_menu, menu.nama,menu.jenis, order_detil.harga, order_detil.jumlah
+        FROM order_detil
+        LEFT JOIN menu ON menu.id_menu = order_detil.id_menu
+        WHERE order_detil.id_order = $id_order
+        ORDER BY order_detil.id_order");
+    $tanggal = $order["tgl_order"];
+    $jam = $order["jam_order"];
+    $no = $order["no_meja"];
+}
+
+$status_order_detil = query("SELECT status_order_detil FROM order_detil WHERE id_order = $id_order");
+$jumlah_selesai = 0;
+$jumlah_diproses = 0;
+
+foreach ($status_order_detil as $status) {
+    if ($status["status_order_detil"] == "selesai") {
+        $jumlah_selesai++;
+    } elseif ($status["status_order_detil"] == "diproses") {
+        $jumlah_diproses++;
+    }
+}
+
+if ($jumlah_selesai == count($order_detail)) {
+    updateStatusOrder($id_order, "Selesai");
+} elseif ($jumlah_selesai > 0 || $jumlah_diproses > 0) {
+    updateStatusOrder($id_order, "Diproses");
+} else {
+    updateStatusOrder($id_order, "Baru");
+}
+
+
+
+if (isset($_GET["hapus_order_detil"])) {
+    $id_order = $_GET["id_order"];
+    $id_order_detil = $_GET["hapus_order_detil"];
+    hapusOrderDetil($id_order_detil);
+    updateTotalBayar($id_order);
+    header("Location: detil_order.php?id_order=" . $id_order);
+}
+
+
+
+if (isset($_POST["id_order_detil"]) && isset($_POST["status"])) {
+    $id_order_detil = $_POST["id_order_detil"];
+    $status = $_POST["status"];
+    $id_order = $_POST["id_order"];
+    updateStatusDetilOrder($id_order_detil, $status);
+
+
+    header("Location: detil_order.php?id_order=" . $id_order);
+}
+
+
+
+$status = ["baru", "diproses", "selesai"]
+
+?>
+<?php include "layout/header.php" ?>
+<div class="container mt-4">
+    <div class="card">
+        <h5 class="card-header">
+            Detail Order
+        </h5>
+        <div class="card-body">
+            <div class="d-flex justify-content-between">
+                <a href="data_order.php" class="btn btn-secondary btn-sm mb-3">Kembali Ke Data Order</a>
+                <a href="form_order_detil.php?orderId=<?= $id_order ?>&tambahlagi=''" class="btn btn-primary btn-sm mb-3">Tambah Order Detil</a>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead class="table-secondary">
+                        <tr>
+                            <th>ID ORDER</th>
+                            <th>TANGGAL ORDER</th>
+                            <th>JAM ORDER</th>
+                            <th>PELAYAN</th>
+                            <th>NO MEJA</th>
+                            <th>TOTAL BAYAR</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?= $order["id_order"] ?></td>
+                            <td><?= $order["tgl_order"] ?></td>
+                            <td><?= $order["jam_order"] ?></td>
+                            <td><?= $order["pelayan"] ?></td>
+                            <td><?= $order["no_meja"] ?></td>
+                            <td><?= formatHarga($order["total_bayar"]) ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="table-responsive mt-4">
+                <table class="table table-hover table-bordered">
+                    <?php if (!empty($order_detail)) : ?>
+                        <thead class="table-secondary">
+                            <tr>
+                                <th>NO</th>
+                                <th>ID ORDER DETIL</th>
+                                <th>NAMA MENU</th>
+                                <th>JENIS MENU</th>
+                                <th>HARGA</th>
+                                <th>JUMLAH</th>
+                                <th>SUB TOTAL</th>
+                                <th>STATUS</th>
+                                <th>AKSI</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $no = 1;
+                            foreach ($order_detail as $order) :
+                            ?>
+                                <tr>
+                                    <td><?= $no++ ?></td>
+                                    <td><?= $order["id_order_detil"] ?></td>
+                                    <td><?= $order["nama"] ?></td>
+                                    <td><?= $order["jenis"] ?></td>
+                                    <td><?= formatHarga($order["harga"]) ?></td>
+                                    <td><?= $order["jumlah"] ?></td>
+                                    <td><?= formatHarga($order["subtotal"]) ?></td>
+                                    <td>
+                                        <form action="" method="post">
+                                            <input type="hidden" value="<?= $id_order ?>" name="id_order">
+                                            <?php if ($order["status_order_detil"] == "baru" || $order["status_order_detil"] == "diproses") : ?>
+                                                <select class="form-select" name="status" onchange="this.form.submit()">
+                                                    <?php foreach ($status as $s) : ?>
+                                                        <option value="<?= $s ?>" <?= ($order["status_order_detil"] == $s) ? "selected" : "" ?>><?= $s ?></option>
+                                                    <?php endforeach; ?>
+                                                </select>
+                                            <?php else : ?>
+                                                <span class="btn btn-success btn-sm">Selesai</span>
+                                            <?php endif; ?>
+                                            <input type="hidden" name="id_order_detil" value="<?= $order["id_order_detil"] ?>">
+                                        </form>
+                                    </td>
+                                    <td>
+                                        <a class="btn btn-danger btn-sm" href="detil_order.php?hapus_order_detil=<?= $order["id_order_detil"] ?>&id_order=<?= $id_order ?>" onclick="return confirm('Apakah anda yakin ingin id order ini?')">
+                                            hapus
+                                        </a>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        <?php else : ?>
+                            <tr>
+                                <td colspan="8">Tidak ada Order detil</td>
+                            </tr>
+                        <?php endif; ?>
+                        </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+</script>
+<?php include "layout/footer.php" ?>
---
+++ kuliah/pertemuan13/functions.php
@@ -0,0 +1,144 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "db_warung";
+
+// koneksi
+$conn = mysqli_connect($server, $username, $password, $database);
+
+if (!$conn) {
+    die("Koneksi gagal : " . mysqli_connect_error($conn));
+}
+
+function query($query): array
+{
+    global $conn;
+    $rows = [];
+    $result = mysqli_query($conn, $query);
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+// ============================ FUNCTIONS MENU ======================
+
+function tambahMenu(array $data): int
+{
+    global $conn;
+    $nama = htmlspecialchars($data["nama"]);
+    $jenis = htmlspecialchars($data["jenis"]);
+    $harga = htmlspecialchars($data["harga"]);
+    $stok = htmlspecialchars($data["stok"]);
+
+    $query = "INSERT INTO menu (nama, jenis, harga, stok)
+                VALUES ('$nama', '$jenis', '$harga', '$stok')";
+    mysqli_query($conn, $query);
+    return mysqli_affected_rows($conn);
+}
+
+
+
+function editMenu(array $data): int
+{
+    global $conn;
+    $id = $_POST["id_menu"];
+    $nama = htmlspecialchars($data["nama"]);
+    $jenis = htmlspecialchars($data["jenis"]);
+    $harga = htmlspecialchars($data["harga"]);
+    $stok = htmlspecialchars($data["stok"]);
+
+    $query = "UPDATE menu SET
+                nama = '$nama',
+                jenis = '$jenis',
+                harga = '$harga',
+                stok = '$stok'
+
+            WHERE id_menu = $id";
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+function hapusMenu($id_menu)
+{
+    global $conn;
+    mysqli_query($conn, "DELETE FROM menu WHERE id_menu = $id_menu");
+    return mysqli_affected_rows($conn);
+}
+
+
+// ====================== FUNCTIONS ORDER ==========================
+
+function tambahOrder($data)
+{
+    global $conn;
+    $id_order = htmlspecialchars($data["id_order"]);
+    $tanggal = htmlspecialchars($data["tanggal_order"]);
+    $jam = htmlspecialchars($data["jam_order"]);
+    $namaPelayan = htmlspecialchars($data["pelayan"]);
+    $noMeja = htmlspecialchars($data["no_meja"]);
+
+    $query = "INSERT INTO `order` (id_order, tgl_order, jam_order, pelayan, no_meja, total_bayar)
+              VALUES ('$id_order', '$tanggal', '$jam', '$namaPelayan', '$noMeja', 0)";
+
+    mysqli_query($conn, $query);
+    return mysqli_affected_rows($conn);
+}
+
+function hapusOrderByOrderId($id_order)
+{
+    global $conn;
+    mysqli_query($conn, "DELETE FROM `order` WHERE id_order = $id_order");
+    return mysqli_affected_rows($conn);
+}
+
+function tambahOrderDetail($id_order, $id_menu, $harga, $jumlah, $subtotal)
+{
+    global $conn;
+    $query = "INSERT INTO `order_detil` (id_order, id_menu, harga, jumlah, subtotal)
+            VALUES ('$id_order', '$id_menu', '$harga', '$jumlah', '$subtotal')";
+    mysqli_query($conn, $query);
+    return mysqli_affected_rows($conn);
+}
+
+function getHargaByIdMenu($id_menu)
+{
+    $harga = query("SELECT harga FROM menu WHERE id_menu = $id_menu")[0];
+    return $harga;
+}
+
+function hapusOrderDetil($id_order_detil)
+{
+    global $conn;
+    mysqli_query($conn, "DELETE FROM order_detil WHERE id_order_detil = $id_order_detil");
+    return mysqli_affected_rows($conn);
+}
+
+function updateStatusDetilOrder($id_order_detil, $status)
+{
+    global $conn;
+    mysqli_query($conn, "UPDATE `order_detil` SET status_order_detil = '$status' WHERE id_order_detil = $id_order_detil");
+    return mysqli_affected_rows($conn);
+}
+
+function updateTotalBayar($id_order)
+{
+    global $conn;
+    mysqli_query($conn, "UPDATE `order` SET total_bayar = (SELECT sum(subtotal) FROM order_detil WHERE id_order = $id_order) WHERE id_order = $id_order");
+
+    return mysqli_affected_rows($conn);
+}
+
+function updateStatusOrder($id_order, $keterangan_status) {
+    global $conn;
+    mysqli_query($conn, "UPDATE `order` SET status_order = '$keterangan_status' WHERE id_order = $id_order");
+}
+
+//========================= OTHER FUNCTIONS ====================
+function formatHarga(float|int|string $harga): int|float|string
+{
+    return "Rp. " . number_format($harga, 2, ",", ".");
+}
